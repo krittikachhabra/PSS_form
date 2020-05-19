048949188599e6290e52481aaeddc46bba12de54
krittikachhabra <39794728+krittikachhabra@users.noreply.github.com>
2020-05-19 15:26:29 +0530

    sql file

---
+++ pss.sql
@@ -0,0 +1,81 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: May 19, 2020 at 09:55 AM
+-- Server version: 5.7.19
+-- PHP Version: 5.6.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pss`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `responses`
+--
+
+DROP TABLE IF EXISTS `responses`;
+CREATE TABLE IF NOT EXISTS `responses` (
+  `age` int(11) NOT NULL,
+  `degree` varchar(40) NOT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `q1` int(11) NOT NULL,
+  `q2` int(11) NOT NULL,
+  `q3` int(11) NOT NULL,
+  `q4` int(11) NOT NULL,
+  `q5` int(11) NOT NULL,
+  `q6` int(11) NOT NULL,
+  `q7` int(11) NOT NULL,
+  `q8` int(11) NOT NULL,
+  `q9` int(11) NOT NULL,
+  `q10` int(11) NOT NULL,
+  `sum` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userresponses`
+--
+
+DROP TABLE IF EXISTS `userresponses`;
+CREATE TABLE IF NOT EXISTS `userresponses` (
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `age` int(11) NOT NULL,
+  `degree` varchar(50) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `q1` int(11) NOT NULL,
+  `q2` int(11) NOT NULL,
+  `q3` int(11) NOT NULL,
+  `q4` int(11) NOT NULL,
+  `q5` int(11) NOT NULL,
+  `q6` int(11) NOT NULL,
+  `q7` int(11) NOT NULL,
+  `q8` int(11) NOT NULL,
+  `q9` int(11) NOT NULL,
+  `q10` int(11) NOT NULL,
+  `sum` int(11) NOT NULL,
+  `time` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`email`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
